12321e538a196011a45e105109879e6d9db965d8
savchenko2277 <jokarikop@gmail.com>
2026-05-13 07:31:55 +0400

    UPD

--- src/js/common.js
+++ src/js/common.js
@@ -5,14 +5,6 @@ import "./blocks.js";
 // Функции
 
 // Единицы высоты (ширины) экрана
-function updateVH() {
-	const { height = window.innerHeight, width = window.innerWidth } = window.visualViewport || {};
-
-	document.documentElement.style.setProperty('--vh', `${height * 0.01}px`);
-	['resize', 'orientationchange'].forEach(event => {
-		window.addEventListener(event, throttle(updateVH, 200), { passive: true });
-	});
-}
 
 // Ширина скроллбара
 const setScrollbarWidth = () => {
